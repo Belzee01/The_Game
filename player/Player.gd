3776696a580019ca1323e0c805af438787d810ac
Kajetan <kajetanlipensky@gmail.com>
2019-04-26 21:19:15 +0200

    changed size of the tiles, streamlined code

--- player/Player.gd
+++ player/Player.gd
@@ -1,9 +1,4 @@
-extends KinematicBody2D
-
-const SPEED = 220
-
-var movedir = Vector2(0, 0)
-var spritedir = "_down"
+extends "res://engine/entity.gd"
 
 func _physics_process(delta):
 	controls_loop()
@@ -11,15 +6,15 @@ func _physics_process(delta):
 	spritedir_loop() 
 	
 	if is_on_wall():
-		if spritedir == "_left" and test_move(transform, Vector2(-1, 0)):
+		if spritedir == "_left" and test_move(transform, dir.left):
 			animation_switch("push")
-		if spritedir == "_right" and test_move(transform, Vector2(1, 0)):
+		if spritedir == "_right" and test_move(transform, dir.right):
 			animation_switch("push")
-		if spritedir == "_down" and test_move(transform, Vector2(0, 1)):
+		if spritedir == "_down" and test_move(transform, dir.down):
 			animation_switch("push")
-		if spritedir == "_up" and test_move(transform, Vector2(0, -1)):
+		if spritedir == "_up" and test_move(transform, dir.up):
 			animation_switch("push")
-	elif movedir != Vector2(0, 0):
+	elif movedir != dir.center:
 		animation_switch("walk")
 	else:
 		animation_switch("idle")
@@ -33,26 +28,7 @@ func controls_loop():
 	movedir.x = -int(LEFT) + int(RIGHT)
 	movedir.y = -int(UP) + int(DOWN)
 	
-func movement_loop():
-	var motion = movedir.normalized() * SPEED
-	move_and_slide(motion, Vector2(0, 0))
-	
-func spritedir_loop():
-	match movedir:
-		Vector2(-1, 0):
-			spritedir = "_left"
-		Vector2(1, 0):
-			spritedir = "_right"
-		Vector2(0, -1):
-			spritedir = "_up"
-		Vector2(0, 1):
-			spritedir = "_down"
-			
-func animation_switch(animation):
-	var new_animation = str(animation, spritedir)
-	if $player_anim.current_animation!= new_animation:
-		$player_anim.play(new_animation)
-	
+
 	
 	
 	
