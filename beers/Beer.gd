71859033d1e652509e9d3314e333395b0fb9f781
Kajetan Lipensky <klipensk@akamai.com>
2019-05-12 09:56:59 +0200

    added code for the alko meter, moved beer script to beers directory

--- beers/Beer.gd
+++ beers/Beer.gd
@@ -1,8 +1,8 @@
 extends Node
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum BEERS {
+		
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
