883f10f28a45fec6d8c3b037db091bc6aa880bd5
Kajetan Lipensky <klipensk@akamai.com>
2019-05-29 15:28:16 +0200

    added beer order clouds

--- beers/beer_utils/beer_util_1.gd
+++ beers/beer_utils/beer_util_1.gd
@@ -8,8 +8,6 @@ var current_beer_volume = volume
 func drink(delta) :
 	current_beer_volume -= 3 * delta
 	dir.alkoValue += 3 * delta
-	print(dir.alkoValue)
-	print(current_beer_volume)
 
 func reset():
 	current_beer_volume = volume
